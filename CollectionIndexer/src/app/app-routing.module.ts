4d3cb7f76837068656c5a06fbeeb4282d78e6d1d
Michiel <michielvh1995@gmail.com>
2023-05-08 14:53:17 +0200

    Added a stub component for adding new cards

--- CollectionIndexer/src/app/app-routing.module.ts
+++ CollectionIndexer/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { CardComponent } from './card/card.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CardDetailComponent } from './card-detail/card-detail.component';
+import { AddCardComponent } from './add-card/add-card.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/cards', pathMatch: 'full' },
   { path: 'detail/:id', component: CardDetailComponent },
   { path: 'cards', component: CardComponent },
-  { path: 'dashboard', component: DashboardComponent } 
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'new-card', component: AddCardComponent } 
 ];
 
 @NgModule({
